0233aee00f92e3403098be729c01eee7af596b44
jimothy8000@gmail.com <jimothy8000@gmail.com>
2023-08-28 10:23:54 -0700

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxNTk1OQ/spectral.js?branch_slug=main&token=5b8b5271-109c-4a2b-bbfb-d1b86dadd6c2";
+export default { extends: ruleset };
---
+++ docs/New-Page.md
@@ -0,0 +1,7 @@
+---
+stoplight-id: 43pjr7282pknm
+---
+
+# New-Page
+
+The beginning of an awesome article...
